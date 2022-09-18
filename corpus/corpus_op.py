9f961e3eff795272ff788fc9f2e6b8af5732575f
Chenct-jonathan <86584322+Chenct-jonathan@users.noreply.github.com>
2022-09-19 01:09:04 +0800

    upload auto-purged files

--- corpus/corpus_op.py
+++ corpus/corpus_op.py
@@ -9,11 +9,11 @@ reDICT = {
     "Jihu":'[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*幾乎\s*[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*'
 }
 
-with open('sinicaCorpus_{}_raw.txt'.format("Jihu"),encoding="utf-8") as f:
+with open('sinicaCorpus_{}_raw.txt'.format("Chiabuduo"),encoding="utf-8") as f:
     lines = ''.join(f.readlines())
-    purge = re.findall(r'{}'.format(reDICT["Jihu"]), lines)
+    purge = re.findall(r'{}'.format(reDICT["Chiabuduo"]), lines)
 
-with open('sinicaCorpus_{}_purge.txt'.format("Jihu"),'w',encoding="utf-8") as g:
+with open('sinicaCorpus_{}_purge.txt'.format("Chiabuduo"),'w',encoding="utf-8") as g:
     for i in range(len(purge)):
         g.write(str(i+1)+" "+purge[i].replace("\t","")+"\n")
 
