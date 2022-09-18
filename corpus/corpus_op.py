8af88c97297bbb91b3b69ba2166a8762f6574db4
Chenct-jonathan <86584322+Chenct-jonathan@users.noreply.github.com>
2022-09-19 00:20:42 +0800

    update corpus.py(auto extract)

--- corpus/corpus_op.py
+++ corpus/corpus_op.py
@@ -3,11 +3,16 @@
 
 import re
 
-with open('sinicaCorpus_Jihu.txt',encoding="utf-8") as f:
+reDICT = {
+    "Chiayidian":"[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*差一點\s*[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*",
+    "Chiabuduo":"[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*差不多\s*[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*",
+    "Jihu":'[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*幾乎\s*[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*'
+}
+with open('sinicaCorpus_{}_raw.txt'.format("Jihu"),encoding="utf-8") as f:
     lines = ''.join(f.readlines())
-    purge = re.findall(r'([０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*幾乎\t*[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*)', lines)
+    purge = re.findall(r'{}'.format(reDICT["Jihu"]), lines)
 
-with open('sinicaCorpus_Jihu_purge.txt','w',encoding="utf-8") as g:
+with open('sinicaCorpus_{}_purge.txt'.format("Jihu"),'w',encoding="utf-8") as g:
     for i in range(len(purge)):
         g.write(str(i+1)+" "+purge[i].replace("\t","")+"\n")
 
