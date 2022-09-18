06807a8395abbaa14a459ff96cd25be589fd60ec
Peter <peter.w@droidtown.co>
2022-09-19 00:17:26 +0800

    調整程式結構

--- corpus/corpus_op.py
+++ corpus/corpus_op.py
@@ -3,13 +3,35 @@
 
 import re
 
-with open('sinicaCorpus_Jihu.txt',encoding="utf-8") as f:
-    lines = ''.join(f.readlines())
-    purge = re.findall(r'([０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*幾乎\t*[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*)', lines)
+def jihu_purger():
+    '''
+    Extract sentences with Jihu(幾乎) from raw Sinica Corpus file, and save it into _purge file.
+    '''
+    with open('sinicaCorpus_Jihu.txt',encoding="utf-8") as f:
+        lines = ''.join(f.readlines())
+        purge = re.findall(r'([０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*幾乎\t*[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*)', lines)
 
-with open('sinicaCorpus_Jihu_purge.txt','w',encoding="utf-8") as g:
-    for i in range(len(purge)):
-        g.write(str(i+1)+" "+purge[i].replace("\t","")+"\n")
+    with open('sinicaCorpus_Jihu_purge.txt','w',encoding="utf-8") as g:
+        for i in range(len(purge)):
+            g.write(str(i+1)+" "+purge[i].replace("\t","")+"\n")
+
+
+def main():
+    resultDICT = {"JiHu_status":True,
+                  "ChaBuDuo_status":True,
+                  "ChaYiDian_status":True
+                  }
+    try:
+        jihu_purger()
+    except:
+        resultDICT["JiHu_status"] = False
+
+
+    return resultDICT
+
+if __name__ == "__main__":
+    main()
+
 
 
 
