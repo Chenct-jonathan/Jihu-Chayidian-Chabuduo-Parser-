4a546abbe38b040db53dc81a337f1aa9e21dc0f6
Chenct-jonathan <86584322+Chenct-jonathan@users.noreply.github.com>
2022-09-19 16:42:15 +0800

    upload auto purger

--- corpus/corpus_op.py
+++ corpus/corpus_op.py
@@ -4,16 +4,16 @@
 import re
 
 reDICT = {
-    "Chiayidian":"[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*差一點\s*[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*",
-    "Chiabuduo":"[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*差不多\s*[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*",
-    "Jihu":'[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*幾乎\s*[０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*'
+    "Chiayidian":'[「」０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*差一點[\s０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％…「」]*',
+    "Chiabuduo":'[「」０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*差不多[\s０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％…「」]*',
+    "Jihu":'[「」０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％]*幾乎[\s０-９ａ-ｚＡ-Ｚ\u4E00-\u9FFF％…「」]*'
 }
 
 with open('sinicaCorpus_{}_raw.txt'.format("Chiabuduo"),encoding="utf-8") as f:
     lines = ''.join(f.readlines())
     purge = re.findall(r'{}'.format(reDICT["Chiabuduo"]), lines)
 
-with open('sinicaCorpus_{}_purge.txt'.format("Chiabuduo"),'w',encoding="utf-8") as g:
+with open('sinicaCorpus_{}_purge02.txt'.format("Chiabuduo"),'w',encoding="utf-8") as g:
     for i in range(len(purge)):
         g.write(str(i+1)+" "+purge[i].replace("\t","")+"\n")
 
